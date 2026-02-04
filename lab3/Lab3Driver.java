70e945b8c596b94a407c8ffa1e30f2b3caaf9025
flamingb096 <flamingb096@gmail.com>
2026-02-04 11:26:16 -0500

    Lab3Driver v1.2

--- lab3/Lab3Driver.java
+++ lab3/Lab3Driver.java
@@ -1,10 +1,23 @@
 package lab3;
 
 import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Lab3Driver {
     public static void printProperties(String fileName, String propertyType) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
 
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            String type = parts[1];
+
+            if (type.equalsIgnoreCase(propertyType)) {
+                System.out.println(line);
+            }
+        }
     }
 
     public static void printPropertiesPrice(String fileName, double maxPrice) throws FileNotFoundException {
