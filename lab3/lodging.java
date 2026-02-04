53db0d2dfcf5b8f3304cf62d7425937e3a6254a7
ThisUserWasFree <zm629534@wne.edu>
2026-02-04 12:10:28 -0500

    added lodging

---
+++ lab3/lodging.java
@@ -0,0 +1,10 @@
+package lab3;
+public class lodging extends Property {
+    public lodging(String mlsNumber, String address, String zip, String location,
+                   int daysOnMarket, double price, int lotSize, String status,
+                   double latitude, double longitude) {
+        super(mlsNumber, address, zip, location, daysOnMarket, price, lotSize, status, latitude, longitude);
+    }
+
+    
+}
