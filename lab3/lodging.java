e075ffcdbe24994b08d6cdb5369a0574b4613f00
ThisUserWasFree <zm629534@wne.edu>
2026-02-04 12:16:00 -0500

    updated lodging's constuctor

--- lab3/lodging.java
+++ lab3/lodging.java
@@ -2,8 +2,14 @@ package lab3;
 public class lodging extends Property {
     public lodging(String mlsNumber, String address, String zip, String location,
                    int daysOnMarket, double price, int lotSize, String status,
-                   double latitude, double longitude) {
+                   double latitude, double longitude, int sqFeet, int yearBuilt, int numBeds, int numBaths, double pricePerSqFoot) {
         super(mlsNumber, address, zip, location, daysOnMarket, price, lotSize, status, latitude, longitude);
+        this.sqFeet = sqFeet:
+        this.yearBuilt = yearBuilt;
+        this.numBeds = numBeds;
+        this.numBaths = numBaths;
+        this.pricePerSqFoot = pricePerSqFoot;
+        
     }
 
     
