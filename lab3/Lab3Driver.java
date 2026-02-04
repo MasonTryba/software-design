dd5e766fe2bb7100eb81da66a28cf281c44525eb
flamingb096 <flamingb096@gmail.com>
2026-02-04 12:45:16 -0500

    Lab3Driver v1.5

--- lab3/Lab3Driver.java
+++ lab3/Lab3Driver.java
@@ -4,8 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.Scanner;
 
+
 public class Lab3Driver {
     public static void printProperties(String fileName, String propertyType) throws FileNotFoundException {
+        /*
+        printProperties reads property data from a file and prints properties of a specified type.
+
+        @params fileName - the name of the file to read property data from
+        @params propertyType - the type of property to filter and print. valid types;
+            "Single Family Residential", "Multi-Family (2-4 Unit)", "Condo/Co-op",
+            "Townhouse", "Vacant Land"
+        @returns void
+        */
         File file = new File("lab3/" + fileName);
         propertyType = '"' + propertyType + '"';
 
@@ -20,12 +30,17 @@ public class Lab3Driver {
                 System.out.println(line);
             }
         }
-
         scanner.close();
     }
 
     public static void printPropertiesPrice(String fileName, double maxPrice) throws FileNotFoundException {
+        /*
+        printPropertiesPrice reads property data from a file and prints properties below a specified price.
 
+        @params fileName - the name of the file to read property data from
+        @params maxPrice - the maximum price to filter and print properties
+        @returns void
+        */
     }
 
     public static void main(String[] args) {
@@ -41,7 +56,7 @@ public class Lab3Driver {
                 "3. 'Condo/Co-op'\n" +
                 "4. 'Townhouse'\n" + 
                 "5. 'Vacant Land'\n" +
-                "Enter choice: "
+                "Enter choice (Full name): "
             );
         String choice = scanner.nextLine();
 
@@ -49,6 +64,7 @@ public class Lab3Driver {
 
         try {
             printProperties(fileName, choice);
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
