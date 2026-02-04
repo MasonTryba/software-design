88edaf4572313f010596a1910e12cc2ae4427fb3
qadome-debug <Oa631032@wne.edu>
2026-02-04 12:45:08 -0500

    SingleFamily subclass created

---
+++ lab3/SingleFamily.java
@@ -0,0 +1,19 @@
+package lab3;
+
+/**
+ * Represents a single-family residential home.
+ */
+public class SingleFamily extends Lodging {
+
+    /**
+     * this will  takes all attributes (Property + Lodging) togther for SingleFamily.
+     */
+    public SingleFamily(String mlsNumber, String address, String zip, String location,
+                        int daysOnMarket, double price, int lotSize, String status,
+                        double latitude, double longitude,
+                        int sqFeet, int yearBuilt, int numBeds, int numBaths, double pricePerSqFoot) {
+
+        super(mlsNumber, address, zip, location, daysOnMarket, price, lotSize, status, latitude, longitude,
+                sqFeet, yearBuilt, numBeds, numBaths, pricePerSqFoot);
+    }
+}
