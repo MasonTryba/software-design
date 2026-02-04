05f48bff89fe78eea313b57fe340b21c67c9e7b5
qadome-debug <Oa631032@wne.edu>
2026-02-04 12:30:16 -0500

    townhouse subclass created

---
+++ Townhouse.java
@@ -0,0 +1,26 @@
+package lab3;
+
+
+public class Townhouse extends Lodging {
+
+    private double hoaFee;
+
+    /**
+     * Constructor that takes all attributes (Property + Lodging + Townhouse).
+     */
+    public Townhouse(String mlsNumber, String address, String zip, String location,
+                     int daysOnMarket, double price, String status,
+                     double latitude, double longitude,
+                     int sqFeet, int yearBuilt, int numBeds, int numBaths, double pricePerSqFoot,
+                     double hoaFee) {
+
+        super(mlsNumber, address, zip, location, daysOnMarket, price,
+                0, status, latitude, longitude,  // lotSize forced to 0
+                sqFeet, yearBuilt, numBeds, numBaths, pricePerSqFoot);
+
+        this.hoaFee = hoaFee;
+    }
+
+    public double getHoaFee() { return hoaFee; }
+    public void setHoaFee(double hoaFee) { this.hoaFee = hoaFee; }
+}
