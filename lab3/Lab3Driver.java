f08a084e7fa2b4095f59339c65d0512600311c3d
flamingb096 <flamingb096@gmail.com>
2026-02-04 11:16:32 -0500

    Lab3Driver v1.1

---
+++ lab3/Lab3Driver.java
@@ -0,0 +1,13 @@
+package lab3;
+
+import java.io.FileNotFoundException;
+
+public class Lab3Driver {
+    public static void printProperties(String fileName, String propertyType) throws FileNotFoundException {
+
+    }
+
+    public static void printPropertiesPrice(String fileName, double maxPrice) throws FileNotFoundException {
+
+    }
+}
