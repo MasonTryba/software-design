40227a92a2900b27c7fa2290d4a6f835e5f8bb88
flamingb096 <flamingb096@gmail.com>
2026-02-04 11:38:38 -0500

    Lab3Driver v1.3

--- lab3/Lab3Driver.java
+++ lab3/Lab3Driver.java
@@ -18,9 +18,37 @@ public class Lab3Driver {
                 System.out.println(line);
             }
         }
+
+        scanner.close();
     }
 
     public static void printPropertiesPrice(String fileName, double maxPrice) throws FileNotFoundException {
 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the property data file name: ");
+        String fileName = scanner.nextLine();
+
+        System.out.println(
+                "Please select a property type to filter\n" + 
+                "1. 'Single Family Residential'\n" +
+                "2. 'Multi-Family (2-4 Unit)'\n" +
+                "3. 'Condo/Co-op'\n" +
+                "4. 'Townhouse'\n" + 
+                "5. 'Vacant Land'\n" +
+                "Enter choice: "
+            );
+        String choice = scanner.nextLine();
+
+        try {
+            printProperties(fileName, choice);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        scanner.close();
     }
 }
