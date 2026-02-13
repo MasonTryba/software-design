3e7dc0b280d3bf5c09cb96208e02cf8f1462314c
flamingb096 <flamingb096@gmail.com>
2026-02-13 10:27:53 -0500

    Small fixes

--- lab4/person.java
+++ lab4/person.java
@@ -1,16 +1,18 @@
 package lab4;
-public class person {
+
+public class Person {
     private String name;
     private String country;
     private int yearBorn;
-    public person(String name, String country, int yearBoarn) {
+
+    public Person(String name, String country, int yearBorn) {
         this.name = name;
         this.country = country;
-        this.yearBorn = yearBoarn;
+        this.yearBorn = yearBorn;
     }
+
     public int getAge() {
         int whatYearIsIt = 2026;
         return whatYearIsIt - this.yearBorn;
     }
-
 }
--- lab4/raceCarDriver.java
+++ lab4/raceCarDriver.java
@@ -1,15 +1,15 @@
 package lab4;
-public class raceCarDriver extends person {
 
+public class RaceCarDriver extends Person {
     private int starts;
     private int winnings;
     private int wins;
 
-    public raceCarDriver(String name, String country, int yearBorn, int starts, int winnings, int wins) {
+    public RaceCarDriver(String name, String country, int yearBorn, int starts, int winnings, int wins) {
         super(name, country, yearBorn);
+
         this.starts = starts;
         this.winnings = winnings;
         this.wins = wins;
     }
-    
 }
