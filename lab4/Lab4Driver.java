fa69039fb0c1ad3a8057b5c60fa16cd53cf87503
flamingb096 <mtryba01@gmail.com>
2026-02-16 12:00:42 -0500

    Lab4Driver

---
+++ lab4/Lab4Driver.java
@@ -0,0 +1,24 @@
+package lab4;
+
+public class Lab4Driver {
+    private Horse sire1;
+    private Horse dam1;
+    private Horse sire2;
+    private Horse dam2;
+
+    private RaceHorse raceHorse1;
+    private RaceHorse raceHorse2;
+    private RaceCarDriver raceCarDriver1;
+
+    public Lab4Driver() {
+        sire1 = new Horse("");
+        dam1 = new Horse();
+        sire2 = new Horse();
+        dam2 = new Horse();
+
+        raceHorse1 = new RaceHorse();
+        raceHorse2 = new RaceHorse();
+        raceCarDriver1 = new RaceCarDriver();
+
+
+}
